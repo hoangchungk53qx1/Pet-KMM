a5c067f12fac3b5cd9e1f73b37ef00eae4f63ead
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2024-07-17 00:25:38 +0700

    add compose utils

---
+++ androidApp/src/main/java/com/vn/chungha/pet_kmm/android/PetApplication.kt
@@ -0,0 +1,9 @@
+package com.vn.chungha.pet_kmm.android
+
+import android.app.Application
+
+class PetApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ androidApp/src/main/java/com/vn/chungha/pet_kmm/android/designsystem/LoadingIndicator.kt
@@ -0,0 +1,20 @@
+package com.vn.chungha.pet_kmm.android.designsystem
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoadingIndicator(modifier: Modifier = Modifier) {
+  Column(
+    modifier = modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    CircularProgressIndicator()
+  }
+}
