4a2da8468e0b28b3a57fb1975a2c91f10f098b59
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2024-07-18 09:52:49 +0700

    init

--- androidApp/src/main/java/com/vn/chungha/pet_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/vn/chungha/pet_kmm/android/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.vn.chungha.pet_kmm.android.presentation.main.PetCodeApp
@@ -16,7 +20,12 @@ class MainActivity : ComponentActivity() {
     WindowCompat.setDecorFitsSystemWindows(window, false)
     enableEdgeToEdge()
     setContent {
-      PetCodeApp()
+      Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+      ) {
+        PetCodeApp()
+      }
     }
   }
 }
---
+++ common/src/commonMain/kotlin/com/vn/chungha/pet_kmm/di/viewModel.kt
@@ -0,0 +1,7 @@
+package com.vn.chungha.pet_kmm.di
+
+import org.koin.dsl.module
+
+val viewModelModule =  module {
+
+}
