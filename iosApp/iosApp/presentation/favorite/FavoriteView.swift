ddb93e5f1d9fe9a25bf0e3ab2f14f6ab382cb86c
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2024-07-21 18:51:41 +0700

    add view nav iOS View

---
+++ iosApp/iosApp/presentation/favorite/FavoriteView.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteView.swift
+//  iosApp
+//
+//  Created by HoangChung on 21/7/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct FavoriteView : View {
+    var body: some View {
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                    Text("Favorite")
+                })
+            }
+        }
+    }
+}
