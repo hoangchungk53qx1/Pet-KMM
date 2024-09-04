ea0e78a525e093d84ef079e9536f9c3d410db978
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2024-09-04 10:04:08 +0700

    load more

--- androidApp/src/main/java/com/vn/chungha/pet_kmm/android/presentation/home/PetCatItemsList.kt
+++ androidApp/src/main/java/com/vn/chungha/pet_kmm/android/presentation/home/PetCatItemsList.kt
@@ -6,31 +6,52 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.vn.chungha.pet_kmm.domain.model.PetModel
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.coroutines.flow.filter
 
 @Composable
 fun PetCatItemsList(
-    items: ImmutableList<PetModel>,
-    isLoading: Boolean,
-    onNextPage: () -> Unit,
-    modifier: Modifier,
-    lazyListState: LazyListState = rememberLazyListState(),
+  items: ImmutableList<PetModel>,
+  isLoading: Boolean,
+  hasReachedMax: Boolean,
+  onNextPage: () -> Unit,
+  modifier: Modifier,
+  lazyListState: LazyListState = rememberLazyListState(),
 ) {
-    LazyColumn(
-        modifier = modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        state = lazyListState,
-    ) {
-        items(items.size) { index ->
-            val item = items[index]
-            PetCatItemRow(
-                petInfo = item,
-                imageHeight = 60.dp,
-                modifier = Modifier
-            )
-        }
+  val rememberOnLoadMore by rememberUpdatedState(onNextPage)
+  val currentHasReachedMax by rememberUpdatedState(hasReachedMax)
+
+  LaunchedEffect(key1 = lazyListState) {
+    snapshotFlow { lazyListState.layoutInfo }
+      .filter {
+        val index = it.visibleItemsInfo.lastOrNull()?.index
+        val totalItemsCount = it.totalItemsCount
+
+        !isLoading && !currentHasReachedMax && index != null && index >= totalItemsCount - 1
+      }
+      .collect {
+        rememberOnLoadMore()
+      }
+  }
+  LazyColumn(
+    modifier = modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+    verticalArrangement = Arrangement.spacedBy(16.dp),
+    state = lazyListState,
+  ) {
+    items(items.size) { index ->
+      val item = items[index]
+      PetCatItemRow(
+          petInfo = item,
+          imageHeight = 60.dp,
+          modifier = Modifier,
+      )
     }
+  }
 }
--- androidApp/src/main/java/com/vn/chungha/pet_kmm/android/presentation/home/PetHomeScreen.kt
+++ androidApp/src/main/java/com/vn/chungha/pet_kmm/android/presentation/home/PetHomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.vn.chungha.pet_kmm.domain.model.PetModel
@@ -15,34 +16,40 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 internal fun PetHomeRoute() {
-    val playerListViewModel = koinViewModel<HomePetViewModel>()
+  val playerListViewModel = koinViewModel<HomePetViewModel>()
 
-    val stateUiPetModel: State<HomePetUiState> =
-        playerListViewModel.statePetModel.collectAsStateWithLifecycle()
+  val stateUiPetModel: State<HomePetUiState> =
+    playerListViewModel.statePetModel.collectAsStateWithLifecycle()
 
-    LaunchedEffect(Unit) {
-        playerListViewModel.getPetFirstList()
+  LaunchedEffect(Unit) {
+    playerListViewModel.getPetFirstList()
+  }
+  PetHomeScreen(
+      modifier = Modifier,
+      items = if (stateUiPetModel.value is HomePetUiState.LoadPageSuccess) {
+          (stateUiPetModel.value as HomePetUiState.LoadPageSuccess).petList
+      } else {
+          emptyList()
+      },
+    onLoadMore = {
+      playerListViewModel.loadNextPage()
     }
-    PetHomeScreen(
-        modifier = Modifier,
-        items = if (stateUiPetModel.value is HomePetUiState.LoadPageSuccess) {
-            (stateUiPetModel.value as HomePetUiState.LoadPageSuccess).petList
-        } else {
-            emptyList()
-        }
-    )
+  )
 }
 
 @Composable
-fun PetHomeScreen(modifier: Modifier,items : List<PetModel>) {
-    val playerListViewModel = koinViewModel<HomePetViewModel>()
-
-    Box(modifier = modifier) {
-        PetCatItemsList(
-            items = items.toImmutableList(),
-            isLoading = false,
-            onNextPage = { playerListViewModel.loadNextPage() },
-            modifier = Modifier
-        )
-    }
+fun PetHomeScreen(
+  modifier: Modifier,
+  items: List<PetModel>,
+  onLoadMore: () -> Unit = {},
+) {
+  Box(modifier = modifier) {
+    PetCatItemsList(
+        items = items.toImmutableList(),
+        isLoading = false,
+        onNextPage = remember { onLoadMore },
+        hasReachedMax = false,
+        modifier = Modifier,
+    )
+  }
 }
